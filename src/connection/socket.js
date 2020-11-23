71f232e8e71c0eac367cae1998dfc76eec8c12ba
Tanveer10071 <mohamedtanveer.n@prodapt.com>
2020-11-23 15:39:36 +0530

    chess frontend

---
+++ src/connection/socket.js
@@ -0,0 +1,21 @@
+import io from 'socket.io-client'
+
+//const URL = 'https://multiplayer-chess-game-app.herokuapp.com/' 
+//tanveer - server url
+const URL = 'http://localhost:8000/' 
+
+const socket = io(URL)
+
+var mySocketId
+// register preliminary event listeners here:
+
+
+socket.on("createNewGame", statusUpdate => {
+    console.log("A new game has been created! Username: " + statusUpdate.userName + ", Game id: " + statusUpdate.gameId + " Socket id: " + statusUpdate.mySocketId)
+    mySocketId = statusUpdate.mySocketId
+})
+
+export {
+    socket,
+    mySocketId
+}
